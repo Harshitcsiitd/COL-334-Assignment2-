ac5562d4bd344be1edf27ec5730da234875b2eb8
Harshit <cs1240836@iitd.ac.in>
2026-09-10 19:24:12 +0530

    final code

--- src/netutil.hpp
+++ src/netutil.hpp
@@ -51,7 +51,7 @@ inline int make_listen_socket(const char* ip, std::uint16_t port) {
         return -1;
     }
     if (!set_nonblocking(fd)) { close(fd); return -1; }
-    if (listen(fd, 4096) < 0) {// changed for extra"
+    if (listen(fd, SOMAXCONN) < 0) {
         perror("listen");
         close(fd);
         return -1;
